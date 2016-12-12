ff9ffd8656aa35a28989e0db6dd112623edb409f
Edwin Eefting <edwin@datux.nl>
2016-12-13 00:14:08 +0100

    wifi and ota stuff

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,6 +1,10 @@
 // meowton - proof of concept for an automatic pet scale
 
 #include "HX711.h"
+#include <ESP8266mDNS.h>
+#include <ESP8266WiFi.h>
+#include "FS.h"
+#include <ArduinoOTA.h>
 
 //moving average smoothing number
 
@@ -36,43 +40,102 @@
 t
 */
 
+
+void OTA_config()
+{
+    ArduinoOTA.onStart([]() {
+        Serial.println("OTA: Start");
+        SPIFFS.end(); //important
+    });
+    ArduinoOTA.onEnd([]() {
+        Serial.println("\nOTA: End");
+        //"dangerous": if you reset during flash you have to reflash via serial
+        //so dont touch device until restart is complete
+        Serial.println("\nOTA: DO NOT RESET OR POWER OFF UNTIL BOOT+FLASH IS COMPLETE.");
+        delay(100);
+        ESP.reset();
+    });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+    ArduinoOTA.onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) Serial.println("OTA: Auth Failed");
+        else if (error == OTA_BEGIN_ERROR) Serial.println("OTA: Begin Failed");
+        else if (error == OTA_CONNECT_ERROR) Serial.println("OTA: Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR) Serial.println("OTA: Receive Failed");
+        else if (error == OTA_END_ERROR) Serial.println("OTA: End Failed");
+    });
+    ArduinoOTA.begin();
+}
+
+void wifi_config()
+{
+    File wifi_config_fh = SPIFFS.open("/wifi.txt", "r");
+    if (!wifi_config_fh) {
+        Serial.println("cannot load wifi config.");
+    }
+    else
+    {
+        String ssid=wifi_config_fh.readStringUntil('\n');
+        String password=wifi_config_fh.readStringUntil('\n');
+
+        Serial.print("Wifi connecting to: ");
+        Serial.println(ssid.c_str());
+        WiFi.begin(ssid.c_str(), password.c_str());
+    }
+}
+
+int last_wifi_status=-1;
+void wifi_status()
+{
+    if (WiFi.status() != last_wifi_status)
+    {
+        last_wifi_status=WiFi.status();
+        if (last_wifi_status==WL_CONNECTED)
+        {
+            Serial.print("Wifi connected IP address: ");
+            Serial.print(WiFi.localIP());
+            Serial.print(", strength: ");
+            Serial.println(WiFi.RSSI());
+        }
+        else
+        {
+            Serial.print("Wifi disconnected, status:");
+            Serial.println(last_wifi_status);
+        }
+
+    }
+}
+
 //////////////////////////////////////////////////////////
 HX711 * scale[4];
 float average[4];
-
-
-
-
-
 float factor[4];
 void setup() {
+    //generic stuff that needs to be in a library some day?
     Serial.begin(115200);
     delay(100);
-    Serial.println("\n\n\n\n\nstarting...");
+    Serial.println("\n\n\n\n\nmeowton starting...");
+
+    if (!SPIFFS.begin())
+        Serial.println("SPIFFS: error while mounting");
+
+    OTA_config();
+    wifi_config();
+
+    //actual meowton stuff
+
     factor[0]=CALIBRATE_FACTOR0;
     factor[1]=CALIBRATE_FACTOR1;
     factor[2]=CALIBRATE_FACTOR2;
     factor[3]=CALIBRATE_FACTOR3;
 
-    //caculate actual factor to get correct weight
-    // for (int s=0; s<4; s++)
-    // {
-    //     factor[s]=factor[s]/CALIBRATED_WEIGHT;
-    // }
-
-    //pinout of the 4 weight modules
-    // scale[0]=new HX711(0,1);
-    // scale[1]=new HX711(2,3);
-    // scale[2]=new HX711(4,5);
-    // scale[3]=new HX711(6,7);
-    // scale[0]=new HX711(1,0);
     scale[0]=new HX711(D6,D7);
     scale[1]=new HX711(D4,D5);
     scale[2]=new HX711(D2,D3);
     scale[3]=new HX711(D0,D1);
-    // scale[2]=new HX711(5,4);
-    // scale[3]=new HX711(7,6);
-    //
 
 
     for (int s=0; s<4; s++)
@@ -99,6 +162,9 @@ void tarre()
 }
 
 void loop() {
+
+    wifi_status();
+    ArduinoOTA.handle();
 
 
     //read all modules and calculate moving average, print raw data needed to calibrate
@@ -146,6 +212,5 @@ void loop() {
             tarre();
         }
     }
-    // delay(100);
 
 }
