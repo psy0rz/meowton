ddade260d45709b401b131b7895a2b5742519d89
Edwin Eefting <edwin@datux.nl>
2017-01-03 17:12:59 +0100

    refactored state data

--- analyser.py
+++ analyser.py
@@ -11,12 +11,15 @@ import pymongo
 import bson.objectid
 from config import db
 import collections
+import matplotlib.pyplot as plt
 
 
 class Scale:
     '''to calculate weights from raw data and do stuff like auto tarring and averaging'''
 
     def __init__(self, calibrate_weight, calibrate_factors):
+
+        self.state={}
 
         self.calibrate_weight=calibrate_weight
         self.calibrate_factors=calibrate_factors
@@ -37,42 +40,43 @@ class Scale:
 
 
         self.sensor_count=len(calibrate_factors)
+
         self.__stable_reset()
 
 
-        self.no_tarre=True
-        self.offsets=[]
+        self.state['no_tarre']=True
+        self.state['offsets']=[]
         for i in range(0, self.sensor_count):
-            self.offsets.append(0)
+            self.state['offsets'].append(0)
 
         #last sensor readings
-        self.current=[]
+        self.state['current']=[]
 
     def __stable_reset(self):
-        self.stable_min=100000000
-        self.stable_max=-100000000
-        self.stable_count=0
-        self.stable_totals=[]
-        self.stable_totals_count=0
+        self.state['stable_min']=100000000
+        self.state['stable_max']=-100000000
+        self.state['stable_count']=0
+        self.state['stable_totals']=[]
+        self.state['stable_totals_count']=0
 
 
         for i in range(0, self.sensor_count):
-            self.stable_totals.append(0)
+            self.state['stable_totals'].append(0)
 
 
     def get_average(self):
         '''gets raw average values since of this stable period'''
         ret=[]
-        for total in self.stable_totals:
-            ret.append(int(total/self.stable_totals_count))
+        for total in self.state['stable_totals']:
+            ret.append(int(total/self.state['stable_totals_count']))
         return(ret)
 
     def get_current(self):
-        return(self.current)
+        return(self.state['current'])
 
     def get_average_count(self):
         '''number of samples the current average is caculated over'''
-        return(self.stable_totals_count)
+        return(self.state['stable_totals_count'])
 
 
     def __stable_measurement(self,sensors):
@@ -80,37 +84,37 @@ class Scale:
 
         weight=self.calibrated_weight(sensors)
 
-        if weight<self.stable_min:
-            self.stable_min=weight
+        if weight<self.state['stable_min']:
+            self.state['stable_min']=weight
 
-        if weight>self.stable_max:
-            self.stable_max=weight
+        if weight>self.state['stable_max']:
+            self.state['stable_max']=weight
 
-        if (self.stable_max - self.stable_min) < self.stable_range:
-            self.stable_count=self.stable_count+1
+        if (self.state['stable_max'] - self.state['stable_min']) < self.stable_range:
+            self.state['stable_count']=self.state['stable_count']+1
         else:
             self.__stable_reset()
 
         #do averaging after skipping the first measurements because of scale drifting and recovery
-        if self.stable_count>=self.stable_skip_measurements:
+        if self.state['stable_count']>=self.stable_skip_measurements:
                 sensor_nr=0
                 for sensor in sensors:
-                    self.stable_totals[sensor_nr]=self.stable_totals[sensor_nr]+sensor
+                    self.state['stable_totals'][sensor_nr]=self.state['stable_totals'][sensor_nr]+sensor
                     sensor_nr=sensor_nr+1
 
-                self.stable_totals_count=self.stable_totals_count+1
+                self.state['stable_totals_count']=self.state['stable_totals_count']+1
 
 
     def measurement(self, sensors):
         self.__stable_measurement(sensors)
-        self.current=sensors
+        self.state['current']=sensors
 
 
         #do auto tarring after a long stable period
-        if (self.stable_totals_count>self.stable_auto_tarre)  or (self.no_tarre and self.stable_totals_count>10):
-            self.offsets=self.get_average()
+        if (self.state['stable_totals_count']>self.stable_auto_tarre)  or (self.state['no_tarre'] and self.state['stable_totals_count']>10):
+            self.state['offsets']=self.get_average()
             self.__stable_reset()
-            self.no_tarre=False
+            self.state['no_tarre']=False
 
 
     def offset(self, sensors):
@@ -118,7 +122,7 @@ class Scale:
         ret=[]
         sensor_nr=0
         for sensor in sensors:
-            ret.append(sensor - self.offsets[sensor_nr])
+            ret.append(sensor - self.state['offsets'][sensor_nr])
             sensor_nr=sensor_nr+1
         return(ret)
 
@@ -138,22 +142,23 @@ class Catalyser():
     '''try to track the weights of several cats during eating. catalyser works with weights instead of raw sensor values'''
 
     def __init__(self):
-        self.average_last=0
-        self.average_prev=0
-        self.average_count=0
-        self.graph_measurements=collections.deque([],3000)
-        self.graph_averages=collections.deque([],3000)
+        self.state={}
+
+        self.state['average_last']=0
+        self.state['average_prev']=0
+        self.state['average_count']=0
+        self.state['graph_measurements']=collections.deque([],3000)
+        self.state['graph_averages']=collections.deque([],3000)
 
         #measure enter and exist weights and eating time
-        self.enter_weight=0
-        self.enter_time=0
-        self.enter_index=0
-        self.exit_weight=0
+        self.state['enter_weight']=0
+        self.state['enter_time']=0
+        self.state['enter_index']=0
+        self.state['exit_weight']=0
+
 
         #per cat data
-        doc=db.settings.find_one({ 'name': 'cats' })
-
-        self.cats=[]
+        self.state['cats']=[]
 
         pass
 
@@ -163,35 +168,35 @@ class Catalyser():
         '''update with current state of scale'''
 
         #keep graph of last measuremenst
-        self.graph_measurements.append(int(scale.calibrated_weight(scale.offset(scale.get_current()))))
-        self.enter_index=self.enter_index+1
+        self.state['graph_measurements'].append(int(scale.calibrated_weight(scale.offset(scale.get_current()))))
+        self.state['enter_index']=self.state['enter_index']+1
 
         #is there a stable average
         if scale.get_average_count():
 
             average_current=int(scale.calibrated_weight(scale.offset(scale.get_average())))
-            self.graph_averages.append(average_current)
+            self.state['graph_averages'].append(average_current)
 
             #the average is still changing
-            if average_current!=self.average_last:
-                self.average_count=0
-                self.average_last=average_current
+            if average_current!=self.state['average_last']:
+                self.state['average_count']=0
+                self.state['average_last']=average_current
             #average stayed the same since last measurement
             else:
-                self.average_count=self.average_count+1
+                self.state['average_count']=self.state['average_count']+1
 
                 #average stayed the same for a number of measurements
-                if self.average_count==100:
+                if self.state['average_count']==100:
 
                     #determine difference with previous stable average
-                    diff=average_current-self.average_prev
-                    self.average_prev=average_current
+                    diff=average_current-self.state['average_prev']
+                    self.state['average_prev']=average_current
 
                     #record the change-event
                     self.__event(diff, timestamp)
 
         else:
-            self.graph_averages.append(None)
+            self.state['graph_averages'].append(None)
 
 
     def __event(self, diff, timestamp):
@@ -203,49 +208,49 @@ class Catalyser():
 
         #cat entered
         if diff>0:
-            self.enter_time=timestamp
-            self.enter_weight=diff
-            self.enter_index=0
+            self.state['enter_time']=timestamp
+            self.state['enter_weight']=diff
+            self.state['enter_index']=0
 
 
         #cat exited
         else:
-            self.exit_weight=abs(diff)
+            self.state['exit_weight']=abs(diff)
 
             errors=[]
 
             #if it took too long we probably missed something
-            timediff=timestamp-self.enter_time
-            consumed=self.exit_weight-self.enter_weight
+            timediff=timestamp-self.state['enter_time']
+            consumed=self.state['exit_weight']-self.state['enter_weight']
 
-            if timestamp-self.enter_time>6000:
+            if timestamp-self.state['enter_time']>6000:
                 errors.append("Cat took too long.".format())
 
             #cat can only stay the same weight or get heavier while eating.
             #if it got much lighter the measurements are wrong
-            if self.exit_weight-self.enter_weight<-2:
+            if self.state['exit_weight']-self.state['enter_weight']<-2:
                 errors.append("Cat got lighter while eating somehow.")
 
-            if self.exit_weight-self.enter_weight>100:
+            if self.state['exit_weight']-self.state['enter_weight']>100:
                 errors.append("Cat ate impossible amount: {}g".format(consumed))
 
             #determine which cat it is
-            cat=self.__find_cat(self.enter_weight)
+            cat=self.__find_cat(self.state['enter_weight'])
             if cat:
                 #update moving average weight to not lose track of cat
-                cat['weight']=int(cat['weight']*0.9 + self.enter_weight*0.1)
+                cat['weight']=int(cat['weight']*0.9 + self.state['enter_weight']*0.1)
             else:
                 #new cat
                 cat={
-                    'weight': self.enter_weight,
-                    'name': "Cat "+str(len(self.cats))
+                    'weight': self.state['enter_weight'],
+                    'name': "Cat "+str(len(self.state['cats']))
                 }
-                self.cats.append(cat)
+                self.state['cats'].append(cat)
 
             print("Date            :", time.ctime(timestamp))
             print("Name            :", cat['name'])
-            print("Cat enter weight:", self.enter_weight)
-            print("Cat exit  weight:", self.exit_weight)
+            print("Cat enter weight:", self.state['enter_weight'])
+            print("Cat exit  weight:", self.state['exit_weight'])
             print("Food consumed   :", consumed)
             print("Eating time     :", timediff)
             print("Errors          :", errors)
@@ -257,8 +262,8 @@ class Catalyser():
             db.events.insert({
                 'name': cat['name'],
                 'timestamp'   : timestamp,
-                'enter_weight': self.enter_weight,
-                'exit_weight' : self.exit_weight,
+                'enter_weight': self.state['enter_weight'],
+                'exit_weight' : self.state['exit_weight'],
                 'timediff'    : timediff,
                 'errors'      : errors,
                 'consumed'    : consumed
@@ -266,25 +271,24 @@ class Catalyser():
 
 
             ######### graph
-            import matplotlib.pyplot as plt
 
-            entered_x=len(self.graph_measurements)-self.enter_index
+            entered_x=len(self.state['graph_measurements'])-self.state['enter_index']
             if entered_x<0:
                 entered_x=0
 
-            exit_x=len(self.graph_measurements)-1
+            exit_x=len(self.state['graph_measurements'])-1
 
             plt.annotate(
-                'Entered at {}g'.format(self.enter_weight),
-                xy=(entered_x, self.graph_measurements[entered_x]),
-                xytext=(entered_x-100, self.graph_measurements[entered_x]-500),
+                'Entered at {}g'.format(self.state['enter_weight']),
+                xy=(entered_x, self.state['graph_measurements'][entered_x]),
+                xytext=(entered_x-100, self.state['graph_measurements'][entered_x]-500),
                 arrowprops=dict(color='gray', width=0.1, headwidth=5),
                 )
 
             plt.annotate(
-                'Exitted at {}g'.format(self.exit_weight),
-                xy=(exit_x, self.graph_measurements[exit_x]),
-                xytext=(exit_x-1000, self.graph_measurements[exit_x]+500),
+                'Exitted at {}g'.format(self.state['exit_weight']),
+                xy=(exit_x, self.state['graph_measurements'][exit_x]),
+                xytext=(exit_x-1000, self.state['graph_measurements'][exit_x]+500),
                 arrowprops=dict(color='gray', width=0.1, headwidth=5),
                 )
 
@@ -298,16 +302,16 @@ class Catalyser():
 
 
             #graphs and labels
-            plt.plot(self.graph_measurements,color='black')
-            plt.plot(self.graph_averages, color='red')
+            plt.plot(self.state['graph_measurements'],color='black')
+            plt.plot(self.state['graph_averages'], color='red')
             plt.ylabel('Weight (g)')
             plt.xlabel('Measurement number')
 
             plt.savefig("graphs/{}.png".format(timestamp))
             plt.close()
 
-            self.enter_weight=0
-            self.enter_time=0
+            self.state['enter_weight']=0
+            self.state['enter_time']=0
 
 
 
@@ -315,7 +319,7 @@ class Catalyser():
     def __find_cat(self, weight):
         '''try to find a cat by weight'''
 
-        for cat in self.cats:
+        for cat in self.state['cats']:
             #max differnce gram for now
             if abs(cat['weight']-weight)<100:
                 return(cat)
