f26992db31fbaf34d3907e7784fdafc7ff6694d7
Edwin Eefting <edwin@datux.nl>
2018-12-21 13:10:12 +0100

    fixes and cleanups

--- micropython/db.py
+++ micropython/db.py
@@ -13,10 +13,16 @@ class Db():
             try:
                 self.display.msg("Uploading...")
                 print("HTTP posting to {}".format(config.db))
-                resp_data = 'measurements,cat={},scale={} weight={},food={}'.format(cat.state.name, config.id, cat.state.weight, cat.ate_session)
-                resp = urequests.post(config.db, data=resp_data)
-                print('HTTP response: {}'.format(resp))
-                self.display.msg("")
+                req_data = 'measurements,cat={},scale={} weight={},food={}'.format(cat.state.name, config.id, cat.state.weight, cat.ate_session)
+                resp=urequests.post(config.db, data=req_data, headers={ 'Content-Type': 'text/plain' })
+
+                print('HTTP response "{}", text: {} '.format(resp.reason,resp.text))
+
+                if resp.status_code!=204:
+                    self.display.msg("Database error.")
+                else:
+                    self.display.msg("")
+
             except Exception as e:
                 print('HTTP error: {}'.format(e))
-                self.display.msg("Upload error.", timeout=None)
+                self.display.msg("Network error.", timeout=None)
--- micropython/main.py
+++ micropython/main.py
@@ -17,6 +17,8 @@ scale_cat=scalecat.ScaleCat(display, cats, db)
 scale_food=scalefood.ScaleFood(display, cats)
 scale_io=scaleio.ScaleIO()
 
+#doing my part :)
+display.msg("Subscribe2Pewdiepie!")
 
 
 import micropython
@@ -58,8 +60,8 @@ def n(name):
 # prev=0
 
 
-# led=machine.Pin(5,machine.Pin.OUT)
-# oldvalue=True
+led=machine.Pin(5,machine.Pin.OUT)
+oldvalue=True
 
 slow_check_timestamp=timer.timestamp
 
@@ -79,16 +81,16 @@ def loop(sched=None):
         scale_food.measurement(f)
 
 
-        # if not wlan.isconnected():
-        #     global oldvalue
-        #     led.value(oldvalue)
-        #     oldvalue=not oldvalue
-        # else:
-        #     led.value(0)
+        if not wlan.isconnected():
+            global oldvalue
+            led.value(oldvalue)
+            oldvalue=not oldvalue
+        else:
+            led.value(0)
 
 
     #stuff that doesnt have to be done every loop
-    if timer.diff(timer.timestamp,slow_check_timestamp)>1000:
+    if timer.diff(timer.timestamp,slow_check_timestamp)>10000:
 
         ### feed?
         if scale_food.should_feed():
@@ -110,24 +112,7 @@ def loop(sched=None):
     micropython.schedule(loop,None)
 
 
-# while True:
-#     loop(0)
-
-
-
-# i = machine.I2C(0, sda=21, scl=22, freq=100000)
-# while True:
-#     print(i.scan())
-
-
-# #hackety hack..will rewrite later
-#
-# print("Booting feeder")
-# import machine
-# import time
 import config
-#
-#
 
 
 
@@ -141,31 +126,6 @@ wlan.connect(config.wifi_essid, config.wifi_password)
 
 
 
-#network.telnet.start()
-#
-#
-#
-#
-#
-# ### webserver
-# from microWebSrv import MicroWebSrv
-#
-# @MicroWebSrv.route('/feed/<amount>')
-# def handlerFeed(httpClient, httpResponse, routeArgs) :
-#     try:
-#         print("Handling feed request, amount=%s" % routeArgs['amount'])
-#
-#         feed(int(routeArgs['amount']))
-#
-#     except Exception as e:
-#         print("FAILED: " + str(e))
-#
-#     servo.duty(0)
-#
-# # start webserver
-# mws = MicroWebSrv() # TCP port 80 and files in /flash/www
-# mws.Start()         # Starts server in a new thread
-
 
 import sys
 def input_thread():
@@ -174,10 +134,9 @@ def input_thread():
         print("JA"+c)
 
 
-import _thread
-# _thread.start_new_thread (input_thread,[])
 
-print("* SUBSCRIBE TO PEWDIEPIE *")
+
+
 # while True:
 #     loop()
 
