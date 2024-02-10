9feace327ca9b27e1786cd3aeb0be75d625c93e9
Edwin Eefting <edwin@datux.nl>
2024-02-10 13:49:39 +0100

    beide

--- src/test.py
+++ src/test.py
@@ -2,7 +2,7 @@
 print("STARTING")
 import threading
 from time import sleep
-from nicegui import ui
+# from nicegui import ui
 
 from RPi import GPIO
 from hx711 import HX711
@@ -12,43 +12,54 @@ from scale import Scale
 GPIO.setwarnings(False)
 
 print("CREAT")
-hx711 = HX711(
+hx711_food = HX711(
     dout_pin=23,
     pd_sck_pin=24,
+    channel='A',
+    gain=128
+)
+hx711_cat = HX711(
+    dout_pin=27,
+    pd_sck_pin=17,
     channel='A',
     gain=128
 )
 
 print("RESET")
 
-hx711.reset()   # Before we start, reset the HX711 (not obligate)
+hx711_cat.reset()   # Before we start, reset the HX711 (not obligate)
+hx711_food.reset()
 
-scale=Scale(1, 'test.json')
-if not scale.is_calibrated():
-    scale.calibrate_weight=10
-    scale.recalibrate()
+scale_cat=Scale(1, 'cat.json')
+if not scale_cat.is_calibrated():
+    scale_cat.calibrate_weight=200
+    scale_cat.recalibrate()
 
+scale_food=Scale(1, 'food.json')
+if not scale_food.is_calibrated():
+    scale_food.calibrate_weight=10
+    scale_food.recalibrate()
 
-label=ui.label("waardee")
 
 def reader():
-    previous=0
+    previous_food=0
+    previous_cat=0
     while True:
-        raw=hx711._read()
-        if raw is not False:
-            # print(raw)
+        raw_food=hx711_food._read()
+        if raw_food is not False:
 
-            if abs(raw-previous)<10000:
-                scale.measurement( [raw])
-            else:
-                print("IGNORED")
-            previous=raw
-        else:
-            print("ERROR")
-        sleep(0.1)
-        label.set_text(f"{scale.last_realtime_weight:.2f}g")
+            if abs(raw_food-previous_food)<10000:
+                scale_food.measurement( [raw_food])
+            previous_food=raw_food
+
+        raw_cat=hx711_cat._read()
+        if raw_cat is not False:
+
+            if abs(raw_cat-previous_cat)<10000:
+                scale_cat.measurement( [raw_cat])
+            previous_cat=raw_cat
 
 
 threading.Thread(target=reader).start()
 print("start ui")
-ui.run(reload=False)
+# ui.run(reload=False)
