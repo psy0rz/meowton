2e1a1296296dfca14bdfcb63579163082f97c6ac
Edwin Eefting <edwin@datux.nl>
2024-04-19 19:24:48 +0200

    stuff

--- src/cat_detector.py
+++ src/cat_detector.py
@@ -1,6 +1,9 @@
+from peewee import fn
+
+from cat import Cat
 from scale import Scale
 
-import nicegui
+MIN_WEIGHT = 100
 
 
 class CatDetector:
@@ -10,7 +13,20 @@ class CatDetector:
         scale.subscribe_stable(self.scale_stable)
         scale.subscribe_unstable(self.scale_unstable)
 
+    def find_closest_weight(self, target_weight):
+
+        if target_weight < MIN_WEIGHT:
+            return None
+
+        query = (Cat.select()
+                 .order_by(fn.Abs(Cat.weight - target_weight))
+                 .limit(1))
+        return query.first()
+
     def scale_stable(self, weight: float):
+        cat: Cat = self.find_closest_weight(weight)
+        if cat is not None:
+            print(f" {cat.name} = {cat.weight}g")
         pass
 
     def scale_unstable(self):
