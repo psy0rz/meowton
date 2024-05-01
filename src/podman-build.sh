78074a0a1519b53e554a94522d044785eb14d531
Edwin Eefting <edwin@datux.nl>
2024-05-01 13:55:21 +0200

    timezone

--- src/podman-build.sh
+++ src/podman-build.sh
@@ -9,7 +9,7 @@ echo "Creating/updating meowton container"
 systemctl stop meowton || true
 podman rm meowton -f || true
 podman build . -t meowton --pull
-podman create -e MATPLOTLIB=false --stop-timeout=0 --network host --privileged --name meowton -v "$PWD":/app meowton
+podman create -e MATPLOTLIB=false --stop-timeout=0 --network host --privileged --name meowton -v /etc/localtime:/etc/localtime:ro -v "$PWD":/app meowton
 
 # add to systemctl
 podman generate systemd -n meowton > /etc/systemd/system/meowton.service
